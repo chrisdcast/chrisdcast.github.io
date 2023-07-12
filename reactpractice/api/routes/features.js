5d5bbfc8ea1a62aedfa36213bcbdfb9758a820cf
chrisdcast <chrisdcast@gmail.com>
2023-07-12 07:39:55 -0500

    Removed Irrelevant react practice
    That project was me just messing around. Nothing lost by getting rid of it

--- reactpractice/api/routes/features.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-router.get('/', (req, res, next) => {
-    res.send('Here is some feature info');
-});
-
-module.exports = router;
--- reactpractice/api/routes/testAPI.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-router.get('/', (req, res, next) => {
-    res.send('API is working properly');
-});
-
-module.exports = router;
